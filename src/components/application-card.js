33a4ebbfb6e2db10d2b12adfa4ae4afaaed2c239
digi0ps <sriru1998@gmail.com>
2018-01-15 10:24:57 +0530

    Added user modal.

--- src/components/application-card.js
+++ src/components/application-card.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import feather from 'feather-icons';
 
+import feather from 'feather-icons';
+import Modal from 'react-responsive-modal';
 import { getUserById } from '../utils/api-helper';
 
 export default class ApplicationCard extends React.Component {
@@ -9,8 +10,19 @@ export default class ApplicationCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: null
+      user: null,
+      open: false
     };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({open: true});
+  }
+
+  closeModal() {
+    this.setState({open: false});
   }
 
   componentDidMount() {
@@ -34,6 +46,17 @@ export default class ApplicationCard extends React.Component {
     return age;
   }
 
+  renderSocialLinks() {
+    const social = this.state.user.social_links;
+    return (
+      <div className="urls">
+        {social.github?<a className="has-text-primary" href={social.github} target="_blank">Github</a>:""}
+        {social.linkedIn?<a className="has-text-primary" href={social.linkedIn} target="_blank">LinkedIn</a>:""}
+        {social.resume?<a className="has-text-primary" href={social.resume} target="_blank">Resume</a>:""}
+      </div>
+    );
+  }
+
   render() {
     const hacker = this.props.hacker;
     const user = this.state.user;
@@ -44,6 +67,8 @@ export default class ApplicationCard extends React.Component {
       );
 
     const age = this.getAge(user.birthday);
+    const social = this.state.user.social_links;
+    window.user = this.state.user;
     // Set application tag class
     let tagClass = "tag ";
     switch(hacker.application_status){
@@ -59,7 +84,7 @@ export default class ApplicationCard extends React.Component {
     }
 
     return (
-      <div className="card user-card">
+      <div className="card user-card" onClick={this.openModal}>
         <div className="card-content">
           <div className="content has-text-centered">
             <p className="is-size-5 is-marginless">{user.first_name + " " + user.last_name}</p>
@@ -71,6 +96,27 @@ export default class ApplicationCard extends React.Component {
             </div>
           </div>
         </div>
+
+        <Modal
+          open={this.state.open}
+          onClose={this.closeModal}
+          closeOnOverlayClick={false}
+          little>
+          <div className="modal-content has-text-centered">
+            <p className="is-size-3 has-text-weight-light has-text-capitalized">{user.first_name + " " + user.last_name}</p>
+            <p className={tagClass+" is-medium"}>{hacker.application_status}</p><br />
+            <div className="user-data">
+              <p className="app-user-data"><strong>Age</strong> {age} yrs </p>
+              <p className="app-user-data"><strong>Location</strong> {user.city} </p>
+              <p className="app-user-data"><strong>University</strong> {user.school_last_attended} </p>
+              <br />
+              <p className="is-size-5">Social Links</p>
+              <div>
+                {!social?"No social links provided.":this.renderSocialLinks()}
+              </div>
+            </div>
+          </div>
+        </Modal>
       </div>
     );
   }
