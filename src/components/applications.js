a80924639793271b821b5eca55f908c027c48bef
digi0ps <sriru1998@gmail.com>
2018-01-25 11:20:21 +0530

    Adds statistics section to applications page.

--- src/components/applications.js
+++ src/components/applications.js
@@ -5,6 +5,7 @@ import feather from 'feather-icons';
 import Navbar from './navbar';
 import Footer from './footer';
 import ApplicationCard from './application-card.js';
+import Statistics from './statistics.js';
 import { getUserProfile, getHackathon, getHackers } from '../utils/api-helper.js';
 
 import '../css/applications.css';
@@ -17,6 +18,7 @@ export default class Applications extends React.Component {
       profile: {},
       hackathon: {},
       hackers: [],
+      stats: {},
       notFound: false
     };
   }
@@ -37,7 +39,28 @@ export default class Applications extends React.Component {
       });
 
     getHackers(hackId)
-      .then(hackers => this.setState({hackers: hackers}));
+      .then(hackers =>
+        this.setState({
+          hackers: hackers,
+          stats: this.calculateStats(hackers)
+        })
+    );
+  }
+
+  calculateStats(hackers) {
+    const stats = {
+      applied: 0, 
+      accepted: 0, 
+      waitlisted: 0, 
+      rejected: 0
+    };
+
+    // Split and join is for removing the hyphen from wait-listed
+    // Since hyphens aren't allowed as variable names
+    for(let i=0;i<hackers.length;i++)
+      stats[hackers[i].application_status.split("-").join("")]++;
+
+    return stats;
   }
 
   render(){
@@ -56,7 +79,8 @@ export default class Applications extends React.Component {
         <section className="section">
           <h1 className="has-text-success is-capitalized has-text-weight-light has-text-centered is-size-3">{hackathon.name}</h1>
           <br />
-
+          <Statistics stats={this.state.stats} />
+          <br />
         {
           /* Accepted / Pending section */
           /* Search bar */
